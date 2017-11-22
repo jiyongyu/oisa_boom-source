84f866a813b1b0ca20e5463383ae3b386232f41d
Christopher Celio <celio@eecs.berkeley.edu>
2017-11-22 15:19:13 -0800

    Demonstrate how to build BOOM using SimSerial.
    This uses the ucb-bar/testchipip to connect BOOM to the riscv-fesvr using
    the Tethered Serial Interface (TSI).
    
    It is way faster than the Debug Transport Module (DTM).

---
+++ src/main/scala/serial/Configs.scala
@@ -0,0 +1,78 @@
+package serial
+
+import chisel3._
+import freechips.rocketchip.config.{Parameters, Config}
+import freechips.rocketchip.coreplex.{WithRoccExample, WithNMemoryChannels, WithNBigCores, WithRV32, WithoutTLMonitors}
+import freechips.rocketchip.devices.tilelink.BootROMParams
+import freechips.rocketchip.diplomacy.LazyModule
+import freechips.rocketchip.tile.XLen
+import testchipip._
+import boom._
+import boom.system.WithNBoomCores
+
+class WithBootROM extends Config((site, here, up) => {
+  case BootROMParams => BootROMParams(
+    contentFileName = s"./bootrom/bootrom.rv${site(XLen)}.img")
+})
+
+class WithExampleTop extends Config((site, here, up) => {
+  case BuildTop => (clock: Clock, reset: Bool, p: Parameters) =>
+    Module(LazyModule(new ExampleTop()(p)).module)
+})
+
+class WithPWM extends Config((site, here, up) => {
+  case BuildTop => (clock: Clock, reset: Bool, p: Parameters) =>
+    Module(LazyModule(new ExampleTopWithPWM()(p)).module)
+})
+
+class WithBlockDeviceModel extends Config((site, here, up) => {
+  case BuildTop => (clock: Clock, reset: Bool, p: Parameters) => {
+    val top = Module(LazyModule(new ExampleTopWithBlockDevice()(p)).module)
+    top.connectBlockDeviceModel()
+    top
+  }
+})
+
+class WithSimBlockDevice extends Config((site, here, up) => {
+  case BuildTop => (clock: Clock, reset: Bool, p: Parameters) => {
+    val top = Module(LazyModule(new ExampleTopWithBlockDevice()(p)).module)
+    top.connectSimBlockDevice(clock, reset)
+    top
+  }
+})
+
+class BoomSerialConfig extends Config(
+  new WithExampleTop ++ 
+  new DefaultBoomConfig ++ new WithNBoomCores(1) ++ new WithoutTLMonitors ++
+  new BaseExampleConfig)
+  
+class BaseExampleConfig extends Config(
+  new WithBootROM ++
+  new freechips.rocketchip.system.DefaultConfig)
+
+class DefaultExampleConfig extends Config(
+  new WithExampleTop ++ new BaseExampleConfig)
+
+class RoccExampleConfig extends Config(
+  new WithRoccExample ++ new DefaultExampleConfig)
+
+class PWMConfig extends Config(new WithPWM ++ new BaseExampleConfig)
+
+class SimBlockDeviceConfig extends Config(
+  new WithBlockDevice ++ new WithSimBlockDevice ++ new BaseExampleConfig)
+
+class BlockDeviceModelConfig extends Config(
+  new WithBlockDevice ++ new WithBlockDeviceModel ++ new BaseExampleConfig)
+
+class WithTwoTrackers extends WithNBlockDeviceTrackers(2)
+class WithFourTrackers extends WithNBlockDeviceTrackers(4)
+
+class WithTwoMemChannels extends WithNMemoryChannels(2)
+class WithFourMemChannels extends WithNMemoryChannels(4)
+
+class DualCoreConfig extends Config(
+  // Core gets tacked onto existing list
+  new WithNBigCores(1) ++ new DefaultExampleConfig)
+
+class RV32ExampleConfig extends Config(
+  new WithRV32 ++ new DefaultExampleConfig)
